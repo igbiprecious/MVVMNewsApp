cd0de215c10dc534b514bc7945eabd1c7db415de
Igbi Precious <igbiprecious@gmail.com>
2021-08-24 12:15:59 +0100

    MVVM News App

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "5ddd20b887e547328e4171c8161b0e0d"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
